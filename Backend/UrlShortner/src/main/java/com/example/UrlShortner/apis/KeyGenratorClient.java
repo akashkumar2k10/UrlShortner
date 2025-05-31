466b1accbc9f794aad12a63edc1bdf403766f6cd
Akash Kumar <akashkumar2k10@gmail.com>
2025-05-31 14:18:48 +0530

    Add ConfigServer deployment and service configuration, update application properties, and enhance UrlShortner with Zipkin integration

--- Backend/UrlShortner/src/main/java/com/example/UrlShortner/apis/KeyGenratorClient.java
+++ Backend/UrlShortner/src/main/java/com/example/UrlShortner/apis/KeyGenratorClient.java
@@ -5,7 +5,7 @@ import com.example.UrlShortner.Models.Payload.DtoKey;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "keygenrator", url = "${KEY_BASE_URL:http://localhost:8080}")
+@FeignClient(name = "KeyGenrator", url = "${KEY_BASE_URL:http://localhost:8080}")
 public interface KeyGenratorClient {
     @GetMapping("keygenrator/api/key") // Add the correct path
     public DtoKey getkey();
